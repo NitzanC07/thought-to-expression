ef9dabfeff8d696641ac740fd3865d643cd55766
Nitzan Cohen <nitzancohen@hotmail.com>
2023-12-04 12:15:27 +0200

    Change application's name

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <section className={styles.container}>
-      <h2>ExpressUs | עמוד פתיחה</h2>
+      <h2>ממחשבה להבעה | עמוד פתיחה</h2>
     </section>
   )
 }
