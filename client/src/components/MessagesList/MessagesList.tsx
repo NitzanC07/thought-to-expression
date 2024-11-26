1e556c5f741c19a6c4d5afd34ac0e222772df5fc
Nitzan Cohen <nitzancohen@hotmail.com>
2024-11-26 22:31:07 +0200

    refresh the working on a project

--- client/src/components/MessagesList/MessagesList.tsx
+++ client/src/components/MessagesList/MessagesList.tsx
@@ -1,28 +1,26 @@
-import styles from './styles.module.scss';
-import { Conversation } from '@/app/conversation/page';
+import styles from "./styles.module.scss";
+import { Conversation } from "@/app/conversation/page";
 
 interface Props {
-    conversation: Conversation[];
+  conversation: Conversation[];
 }
 
-function MessagesList({conversation}: Props) {
-    // const conversation: Conversation[] = conversationData.conversation;
+function MessagesList({ conversation }: Props) {
+  // const conversation: Conversation[] = conversationData.conversation;
 
-    return (
-        <div className={styles.messagesBox}>
-            <ul className={styles.messagesContainer}>
-                {
-                    conversation.map((message, i) => (
-                        <li className={styles.messageContainer} key={i}>
-                            <p className={styles.messageSpeaker}>{message.speaker}:</p>
-                            <p className={styles.messageText}>{message.message}</p>
-                            <p className={styles.messageTime}>{message.time.slice(12, 19)}</p>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    )
+  return (
+    <div className={styles.messagesBox}>
+      <ul className={styles.messagesContainer}>
+        {conversation.map((message, i) => (
+          <li className={styles.messageContainer} key={i}>
+            <p className={styles.messageSpeaker}>{message.speaker}:</p>
+            <p className={styles.messageText}>{message.message}</p>
+            <p className={styles.messageTime}>{message.time.slice(12, 23)}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default MessagesList;
